4b51144ab35c13acebb4a7fee965be79e79c25af
anjaliii-prog <anjali.37679@stu.upes.ac.in>
2026-09-02 15:21:02 +0530

    Add files via upload

---
+++ day4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+int n, sum;
+
+printf("Enter the number : ");
+scanf("%d" , &n );
+
+sum = n*(n+1)/2;
+
+printf("The sum of first n natural number : %d\n", sum );
+
+
+return 0;
+}
